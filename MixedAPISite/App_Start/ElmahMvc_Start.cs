caea0bc98fd949f5c27977796d7928da33beb1ca
Steve Bering <stevebering@gmail.com>
2012-06-26 07:05:55 -0700

    got elmah working with webapi

--- MixedAPISite/App_Start/ElmahMvc_Start.cs
+++ MixedAPISite/App_Start/ElmahMvc_Start.cs
@@ -1,3 +1,5 @@
+using System.Web.Http;
+using MixedAPISite.Models;
 [assembly: WebActivator.PreApplicationStartMethod(typeof(MixedAPISite.App_Start.ElmahMvc), "Start")]
 namespace MixedAPISite.App_Start
 {
@@ -6,6 +8,9 @@ namespace MixedAPISite.App_Start
         public static void Start()
         {
             Elmah.Mvc.Bootstrap.Initialize();
+
+            // set up elmah to handle errors on web api requests as well.
+            GlobalConfiguration.Configuration.Filters.Add(new ElmahErrorAttribute());
         }
     }
 }
