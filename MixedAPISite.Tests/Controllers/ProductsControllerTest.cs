28fa23335c915e8d9f8dcfc543742e2329596138
Steve Bering <sbering@meracord.com>
2013-01-30 13:55:45 -0800

    Expanded testing of products controller

--- MixedAPISite.Tests/Controllers/ProductsControllerTest.cs
+++ MixedAPISite.Tests/Controllers/ProductsControllerTest.cs
@@ -10,8 +10,7 @@ namespace MixedAPISite.Tests.Controllers
     public class ProductsControllerTest
     {
         [Fact]
-        public void GetAllProducts()
-        {
+        public void GetAllProducts() {
             var mock = new Mock<IProductRepository>();
 
             var controller = new ProductsController(mock.Object);
@@ -21,22 +20,44 @@ namespace MixedAPISite.Tests.Controllers
         }
 
         [Fact]
-        public void GetProductById()
-        {
+        public void GetProductById_should_call_get_on_product_repository() {
+            // arrange 
             const int productId = 1;
             var mock = new Mock<IProductRepository>();
 
             mock.Setup(r => r.Get(productId)).Returns(new Product());
 
             var controller = new ProductsController(mock.Object);
+
+            // act
             var result = controller.GetProductById(1);
 
+            // assert
             mock.Verify(r => r.Get(productId));
         }
 
         [Fact]
-        public void GetProductById_should_throw_if_no_product_found()
-        {
+        public void GetProductById_should_return_product_from_repository() {
+            // arrange 
+            const int productId = 1;
+            var mock = new Mock<IProductRepository>();
+
+            var expected = new Product();
+
+            mock.Setup(r => r.Get(productId)).Returns(expected);
+
+            var controller = new ProductsController(mock.Object);
+
+            // act
+            var result = controller.GetProductById(1);
+
+            // assert
+            Assert.Equal(expected, result);
+        }
+
+
+        [Fact]
+        public void GetProductById_should_throw_if_no_product_found() {
             const int productId = 1;
             var mock = new Mock<IProductRepository>();
 
@@ -47,13 +68,10 @@ namespace MixedAPISite.Tests.Controllers
 
             HttpResponseException e = Assert.Throws<HttpResponseException>(() => controller.GetProductById(1));
             Assert.Equal(e.Response.StatusCode, System.Net.HttpStatusCode.NotFound);
-
-            mock.Verify(r => r.Get(productId));
         }
 
         [Fact]
-        public void GetProductsByCategory()
-        {
+        public void GetProductsByCategory() {
             const string categoryName = "any";
 
             var mock = new Mock<IProductRepository>();
